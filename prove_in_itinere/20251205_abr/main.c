32871bc828b856981fb7be31e8a4c9f2dfc4b67e
Andrea Della Maggiora <a.dellamaggiora@caen.it>
2026-05-13 17:42:49 +0200

    changed main

--- prove_in_itinere/20251205_abr/main.c
+++ prove_in_itinere/20251205_abr/main.c
@@ -5,23 +5,39 @@
 #include <errno.h>    // richiesto per usare errno dentro termina()
 
 
-int main(int argc, char *argv[])
-{
-
-  if(argc!=2) {
-    printf("Uso: %s nomefile\n",argv[0]);
-    exit(1);
-  }
-  FILE *f = fopen(argv[1],"r");
-  if(f==NULL) termina("Errore apertura file");
-  fprintf(stdout, "Uso: %s file\n", argv[1]);
-}
-
-void termina(const char *messaggio)
-{
+void termina(const char *messaggio) {
   if(errno==0) 
      fprintf(stderr,"%s\n",messaggio);
   else 
     perror(messaggio);
   exit(1);
+}
+
+
+// argc è un intero che indica il numero di parametri passato (lunghezza dell'array argv)
+// argv[0] è il nome con cui è stato avviato il programma
+int main(int argc, char *argv[]) {
+
+  if(argc!=2) {
+    printf("Uso: %s nomefile\n",argv[0]);
+    exit(1);
+  }
+
+  FILE *f = fopen(argv[1], "r");
+
+  if (f == NULL) {
+    termina("Errore apertura file");
+  }
+
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t read;
+
+  while ((read = getline(&line, &len, f)) != -1) {
+    printf("%zd\n", read);
+    printf("%s", line);
+  }
+
+  free(line);
+  fclose(f);
 }
