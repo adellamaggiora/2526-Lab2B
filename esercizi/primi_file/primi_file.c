20ff2ef8f249aa6cda2915fd4d929c4620ef4979
obenmaarouf <o.benmaarouf@studenti.unipi.it>
2026-05-28 20:01:57 +0200

    code scaffolding

--- esercizi/primi_file/primi_file.c
+++ esercizi/primi_file/primi_file.c
@@ -3,36 +3,75 @@
 #include <pthread.h>à
 #include <semaphore.h>
 
-pthread_mutex_t mutex; //per accesso in sezione critica in mutua esclusione
-//pthread_mutex_lock(&m);
-//pthread_mutex_unlock(&m);
+int *buffer = NULL; 
+int buffer_index = 0;
+int prime_counter = 0;
+int buffer_size = 0;
+pthread_mutex_t mutex_buffer;//per accesso al buffer e all'indice
 
-//sem_t s; 
-//sem_wait(&s);
-//sem_post(&s);
+typedef struct {
+    int *numbers;
+    int length;
+} BufferInfo;
 
-int *read_numbers(char *file_name) {
+BufferInfo read_numbers(char *file_name) {
     FILE *file = fopen(file_name, "r");
-    int *result = NULL;
-    int line_number = 0;
+
+    BufferInfo info;
+    info.numbers = NULL;
+    info.length = 0;
+
     char *line = NULL;
     size_t len = 0;
-    ssize_t read;
-    while((read = getline(line, &len, file)) != -1) {
-        result = realloc(result, (line_number + 1) * sizeof(int));
-        result[line_number] = atoi(line);
+
+    while (getline(&line, &len, file) != -1) {
+        info.numbers = realloc(info.numbers, (info.length + 1) * sizeof(int));
+        info.numbers[info.length] = atoi(line);
+        info.length++;
     }
-    // *line, &line, line
-    return result;
+
+    free(line);
+    fclose(file);
+
+    return info;
 }
 
 int count_prime_numbers() {
+    while(buffer_index < buffer_size){
+        pthread_mutex_lock(&mutex_buffer);
+        int number = buffer[buffer_index];
+        buffer_index++;
+        pthread_mutex_unlock(&mutex_buffer);
+        
+        if(is_prime(number)) {
+            prime_counter++;
+        }  
+    }
 
+}
+
+int is_prime(int n) {
+    if (n < 2) return 0;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return 0;
+    }
+
+    return 1;
 }
 
 int main(int argc, char *argv[]) {
     char *file_name = argv[1];
-    int *numbers = read_numbers(file_name);
+    BufferInfo buffer_info = read_numbers(file_name);
+    buffer = buffer_info.numbers;
+    buffer_size = buffer_info.length;
+    
+    pthread_t p1, p2;
+    pthread_create(&p1, NULL, count_prime_numbers, NULL);
+    pthread_create(&p2, NULL, count_prime_numbers, NULL);
+    pthread_join(&p1, NULL);
+    pthread_join(&p2, NULL);
+    printf("total:%d", prime_counter);
     return 0;
 }
 
