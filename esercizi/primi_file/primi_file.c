3c865a46b95f5c121b8d3803bf0415daf5b9e9e7
adm91 <adm91@email.com>
2026-05-29 12:10:13 +0200

    working code

--- esercizi/primi_file/primi_file.c
+++ esercizi/primi_file/primi_file.c
@@ -1,81 +1,98 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>à
-#include <semaphore.h>
+    #include <stdio.h>
 
-int *buffer = NULL; 
-int buffer_index = 0;
-int prime_counter = 0;
-int buffer_size = 0;
-pthread_mutex_t mutex_buffer;//per accesso al buffer e all'indice
-pthread_mutex_t mutex_counter; 
+    #include <stdlib.h>
 
-typedef struct {
-    int *numbers;
-    int length;
-} BufferInfo;
+    #include <pthread.h>
 
-BufferInfo read_numbers(char *file_name) {
-    FILE *file = fopen(file_name, "r");
+    #include <semaphore.h>
 
-    BufferInfo info;
-    info.numbers = NULL;
-    info.length = 0;
+    int * buffer = NULL;
+    int buffer_index = 0;
+    int prime_counter = 0;
+    int buffer_size = 0;
 
-    char *line = NULL;
-    size_t len = 0;
+    pthread_mutex_t mutex_index = PTHREAD_MUTEX_INITIALIZER;
+    pthread_mutex_t mutex_counter = PTHREAD_MUTEX_INITIALIZER;
 
-    while (getline(&line, &len, file) != -1) {
-        info.numbers = realloc(info.numbers, (info.length + 1) * sizeof(int));
-        info.numbers[info.length] = atoi(line);
-        info.length++;
+    typedef struct {
+       int * numbers;
+       int length;
     }
+    BufferInfo;
 
-    free(line);
-    fclose(file);
-
-    return info;
-}
-
-int count_prime_numbers() {
-    // accesso esclusivo all'indice 
-    pthread_mutex_lock(&mutex_buffer);
-    int current_index = buffer_index;
-    buffer_index++;
-    pthread_mutex_unlock(&mutex_buffer);
-
-    while(current_index < buffer_size){  
-        if(is_prime(buffer[current_index])) {
-            pthread_mutex_lock(&mutex_counter);
-            prime_counter++;
-            pthread_mutex_unlock(&mutex_counter);
-        }
-    }
-
-}
-
-int is_prime(int n) {
-    if (n < 2) return 0;
-
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) return 0;
-    }
-
-    return 1;
-}
-
-int main(int argc, char *argv[]) {
-    char *file_name = argv[1];
-    BufferInfo buffer_info = read_numbers(file_name);
-    buffer = buffer_info.numbers;
-    buffer_size = buffer_info.length;
     
-    pthread_t p1, p2;
-    pthread_create(&p1, NULL, count_prime_numbers, NULL);
-    pthread_create(&p2, NULL, count_prime_numbers, NULL);
-    pthread_join(&p1, NULL);
-    pthread_join(&p2, NULL);
-    printf("total:%d", prime_counter);
-    return 0;
-}
+    BufferInfo read_numbers(char * file_name) {
+       FILE * file = fopen(file_name, "r");
 
+       BufferInfo info;
+       info.numbers = NULL;
+       info.length = 0;
+
+       char * line = NULL;
+       size_t len = 0;
+
+       while (getline( & line, & len, file) != -1) {
+          info.numbers = realloc(info.numbers, (info.length + 1) * sizeof(int));
+          info.numbers[info.length] = atoi(line);
+          info.length++;
+       }
+
+       free(line);
+       fclose(file);
+
+       return info;
+    }
+
+
+    int is_prime(int n) {
+       if (n < 2) return 0;
+       for (int i = 2; i * i <= n; i++) {
+          if (n % i == 0) return 0;
+       }
+       return 1;
+    }
+    
+
+    void *count_prime_numbers(void *arg) {
+
+        while(1) {
+
+            pthread_mutex_lock(&mutex_index);
+            if(buffer_index == buffer_size) {
+                pthread_mutex_unlock(&mutex_index);  // prima sblocca
+                break;
+            }
+
+            int current_element = buffer[buffer_index];
+            buffer_index++;
+            pthread_mutex_unlock(&mutex_index);
+
+            if(is_prime(current_element)) {
+                pthread_mutex_lock(&mutex_counter);
+                prime_counter++;
+                pthread_mutex_unlock(&mutex_counter);
+            }
+        }
+
+        return NULL;
+    }
+
+
+    int main(int argc, char * argv[]) {
+
+       char * file_name = argv[1];
+       BufferInfo buffer_info = read_numbers(file_name);
+       buffer = buffer_info.numbers;
+       buffer_size = buffer_info.length;
+       pthread_t p1, p2;
+
+       pthread_create(&p1, NULL, count_prime_numbers, NULL);
+       pthread_create(&p2, NULL, count_prime_numbers, NULL);
+
+       pthread_join(p1, NULL);
+       pthread_join(p2, NULL);
+
+       printf("total prime numbers: %d \n", prime_counter);
+       free(buffer_info.numbers);
+       return 0;
+    }
